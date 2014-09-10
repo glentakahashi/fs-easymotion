6427f30ba95e90e3ccb313e71c286ccbf165d40e
Glen Takahashi <gtakahashi@palantir.com>
2014-09-10 12:28:13 -0400

    Fixed bug with paths >52 chars long, and added preference for showing root as A

--- cdmotion.sh
+++ cdmotion.sh
@@ -1,8 +1,8 @@
 #change this to 1 if you would prefer to replace first character instead of add it
 REPLACE_FIRST_CHAR=1
+SHOW_ROOT_AS_A=1
 
 #TODO: make customizable and more easily configuraable
-#TODO: add making root (/) as 'a' a preference
 #TODO: also list folders in current directory as letters - maybe make this ,, (a different function)
 
 chr() {
@@ -15,43 +15,52 @@ ord() {
 
 supercd() {
   local DIR=$1
-  local j=$(($2+2))
+  local j=$(($2+1+(1-$SHOW_ROOT_AS_A)))
   for (( i=0; i<${#DIR}; i++ )); do
     CHAR=${DIR:$i:1}
     if [[ $CHAR = "/" ]]; then
       ((j--))
     fi
     if [[ $j -eq 0 ]]; then
-      cd ${DIR:0:$i}
-      return;
+      cd ${DIR:0:$i+1}
+      return 0;
     fi
   done
 }
 
 cdmotion() {
-  #current dir
   local DIR=$(pwd)
 
   #color and settings variables
   #TODO: make customizable
-  local special=$'\e[1m\e[91m'
-  local normal=$'\e[0m\e[39m'
+  local RED_BOLD_FONT=$'\e[1m\e[91m'
+  local NORMAL_FONT=$'\e[0m\e[39m'
 
   #copy dir and change each letter after / into a letter
   #TODO: make this work for paths longer than 52 chars
   local DIR2=$DIR
   local j=0
+  if [[ $SHOW_ROOT_AS_A -eq 1 ]]; then
+    DIR2=$RED_BOLD_FONT'a'$NORMAL_FONT$DIR
+    j=1
+  fi
   for (( i=0; i<${#DIR2}; i++ )); do
     local CHAR=${DIR2:$i:1}
     if [[ $CHAR = "/" ]]; then
       #TODO: refactor
-      if [[ j -lt 26 ]]; then
-        DIR2=${DIR2:0:$i+1}$special$(chr $(($j+97)))$normal${DIR2:$i+1+$REPLACE_FIRST_CHAR:${#DIR2}-$i}
-      elif [[ j -lt 52 ]]; then
-        DIR2=${DIR2:0:$i+1}$special$(chr $(($j+65)))$normal${DIR2:$i+1+$REPLACE_FIRST_CHAR:${#DIR2}-$i}
+      if [[ $j -lt 52 ]]; then
+        local HEAD=${DIR2:0:$i+1}$RED_BOLD_FONT
+        local TAIL=$NORMAL_FONT${DIR2:$i+1+$REPLACE_FIRST_CHAR:${#DIR2}-$i}
+        local CHAR='Z'
+        if [[ $j -lt 26 ]]; then
+          CHAR=$(chr $(($j+97)))
+        else
+          CHAR=$(chr $(($j-26+65)))
+        fi
+        DIR2=$HEAD$CHAR$TAIL
       else
-        >&2 echo Path is too long!
-        return;
+        >&2 echo Path is too long! Must be less than 52 dirs deep.
+        return 1;
       fi
       ((j++))
     fi
@@ -61,22 +70,23 @@ cdmotion() {
     #TODO: make work with bash or zsh
     builtin read -s -k 1 key
     local keyint=$(ord $key)
+    echo $keyint
     if [[ $keyint -ge 97 ]]; then
       if [[ $(($keyint - 97)) -lt j ]]; then
         supercd $DIR $(($keyint - 97))
-        return;
+        return 0;
       else
         echo $DIR2
       fi
-    elif [[ $keyint -ge 66 ]]; then
+    elif [[ $keyint -ge 65 ]]; then
       if [[ $(($keyint - 65 + 26)) -lt j ]]; then
         supercd $DIR $(($keyint - 65 + 26))
-        return;
+        return 0;
       else
         echo $DIR2
       fi
     elif [[ $keyint -eq 27 ]]; then
-      return;
+      return 0;
     else
       echo $DIR2
     fi
